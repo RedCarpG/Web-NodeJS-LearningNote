6253d973b8646dc1625fb17f018b0388c85f7db4
RedCarpG <redcarpg@gmail.com>
2022-01-05 13:19:37 +0100

    add: add main app

---
+++ app.js
@@ -0,0 +1,60 @@
+/** Requirements */
+const createError = require('http-errors');
+const express = require('express'); // Express
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bycrypt = require('bcrypt'); // Bycrtpt
+const passport = require('passport');
+/* Local Requirements */
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
+const initializePassport = require('./public/javascripts/passport-config');
+// initializePassport(
+//   passport,
+//   email => users.find(user => user.email === email)
+// );
+
+const app = express();
+
+/** view engine setup */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+/* Add static path */
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+var port = 8080;
+app.listen(port);
+console.log('Server running at http://127.0.0.1:' + port + '/');
+
+
+module.exports = app;
